0994c7d669692df54feb2161ac85a4f7dcfb2788
coding-rev <emmowu10@gmail.com>
2023-01-27 00:18:44 +0000

    feat: registered models to admin page

--- loan/admin.py
+++ loan/admin.py
@@ -1,3 +1,11 @@
+# Python/django imports
 from django.contrib import admin
+# Local apps imports
+from .models.country_model import Country
+from .models.loan_model import Loan
+from .models.sector_model import Sector
 
-# Register your models here.
+# Register app to admin dashboard
+admin.site.register(Country)
+admin.site.register(Loan)
+admin.site.register(Sector)
