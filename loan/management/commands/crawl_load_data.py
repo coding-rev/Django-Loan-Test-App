aa82a32258868e23bc410e5bff2dbcf0ae76ffa7
coding-rev <emmowu10@gmail.com>
2023-01-27 00:40:05 +0000

    feat: crawl data management command modified

--- loan/management/commands/crawl_load_data.py
+++ loan/management/commands/crawl_load_data.py
@@ -1,5 +1,6 @@
-# Django imports
+# Python/django imports
 from django.core.management import BaseCommand
+from datetime import datetime
 
 # Local imports
 from loan.utils.site_crawler import crawl_site
@@ -14,22 +15,33 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("crawling....")
         try:
-            # get crawled data
+            # Get crawled data
             data = crawl_site()
 
+            # List definition for loan model instances
             loan_data_model_list = []
+
             for index, row in enumerate(data[1:101]):
                 cleaned_row = row.text.split("\n")
+
+                # Get or create country
+                row_country = Country.objects.get_or_create(name=cleaned_row[2])
+                # Get or create sector
+                row_sector = Sector.objects.get_or_create(name=cleaned_row[3])
+                # Sanitize row date to python date format
+                row_signature_date = datetime.strptime(cleaned_row[0], "%d %B %Y")
+
+                # Add current row data to loan model
                 loan_data_model_list.append(
                     Loan(
-                        signature_date=cleaned_row[0],
+                        signature_date=row_signature_date,
                         title=cleaned_row[1],
-                        country=cleaned_row[2],
-                        sector=cleaned_row[3],
+                        country=row_country,
+                        sector=row_sector,
                         signed_amount=cleaned_row[4],
                     )
                 )
-            # Finally make a builk create/save to db
+            # Finally make a loan bulk create
             Loan.objects.bulk_create(loan_data_model_list)
 
             self.stdout.write(
